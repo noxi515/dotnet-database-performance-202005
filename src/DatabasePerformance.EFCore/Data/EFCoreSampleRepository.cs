5b827a04084675e4e118fde12826d2fc80cea690
noxi515 <tzo.ppl@gmail.com>
2020-05-03 02:37:45 +0900

    EFCoreの中身書いてなかった

--- src/DatabasePerformance.EFCore/Data/EFCoreSampleRepository.cs
+++ src/DatabasePerformance.EFCore/Data/EFCoreSampleRepository.cs
@@ -5,9 +5,17 @@ namespace DatabasePerformance.Data
 {
     public class EFCoreSampleRepository : IEFCoreSampleRepository
     {
+        private readonly DatabaseContext _context;
+
+        public EFCoreSampleRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
         public async ValueTask AddAsync(IEnumerable<SampleEntity> data)
         {
-            
+            await _context.AddRangeAsync(data);
+            await _context.SaveChangesAsync();
         }
     }
 }
