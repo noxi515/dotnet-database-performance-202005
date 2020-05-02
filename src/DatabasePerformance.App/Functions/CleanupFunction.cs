00f7f4b5df9b471b37dc26e4a327dc7c7f05ae2b
noxi515 <tzo.ppl@gmail.com>
2020-05-03 02:22:49 +0900

    Cleanup追加

---
+++ src/DatabasePerformance.App/Functions/CleanupFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Dapper;
+using DatabasePerformance.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace DatabasePerformance.Functions
+{
+    public class CleanupFunction
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public CleanupFunction(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        [FunctionName("GetCleanup")]
+        public async Task<ActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "cleanup")] HttpRequest request)
+        {
+            await using var conn = _sqlConnectionFactory.Create();
+            await conn.ExecuteAsync("DELETE FROM sample;");
+
+            return new OkResult();
+        }
+    }
+}
